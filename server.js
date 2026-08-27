7820f974f1adcf66fd19b186446a53b0c8e10d5c
tvgDev <thiago.guimaraaes.dev@gmail.com>
2026-08-27 16:08:39 -0300

    fix: API

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ fastify.post(
 
     try {
       browser = await puppeteer.launch({
-        headless: "new",
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
